c2e0d70ebb39a5f25e349e648faca7cfc430240d
Leszek Miotk <tgb.dc7@gmail.com>
2023-04-27 00:03:42 +0200

    refactor structure

--- app/api/exchangerates.py
+++ app/api/exchangerates.py
@@ -1,11 +1,39 @@
 import requests
-from datetime import datetime
 from fastapi import APIRouter
+from collections import Counter
+from datetime import date
 
 router = APIRouter(prefix="/exchangerates")
 
 
-@router.get("/{code}/{data}")
-def read_exchanges(code: str, date: datetime.date):
-    req = requests.get(f'http://api.nbp.pl/api/exchangerates/rates/A/{code}/{date}/?format=json')
-    return req
+@router.get("/average/{code}/{date}")
+def read_average(code: str, date: date):
+    url = f'http://api.nbp.pl/api/exchangerates/rates/A/{code}/{date}/?format=json'
+    req = requests.get(url)
+    response = req.json()
+    avg = response['rates'][0]['mid']
+    res = {"average": avg}
+    return res
+
+
+@router.get("/minmax/{code}/{topCount}")
+def read_minmax(code: str, topCount: int):
+    url = f"http://api.nbp.pl/api/exchangerates/rates/a/{code}/last/{topCount}/?format=json"
+    req = requests.get(url)
+    response = req.json()
+    minmax_list = [x['mid'] for x in response['rates']]
+    res = {'min': min(minmax_list), 'max': max(minmax_list)}
+    return res
+
+
+@router.get("/difference/{code}/{topCount}")
+def read_difference(code: str, topCount: int):
+    url = f"http://api.nbp.pl/api/exchangerates/rates/c/{code}/last/{topCount}/?format=json"
+
+    req = requests.get(url)
+    response = req.json()
+    difference: list = [round(x['ask'] - x['bid'], 4) for x in response['rates']]
+    max_diff = max(difference)
+    most_common_diff = Counter(difference).most_common(1)[0][0]
+    res = {"max difference": max_diff, "most common difference": most_common_diff}
+    return res
--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
-import requests
-from collections import Counter
-from datetime import date
 from app.core.config import get_app_settings
+from app.api import exchangerates
 
 
 def get_application() -> FastAPI:
@@ -13,41 +11,9 @@ def get_application() -> FastAPI:
 
 
 app = get_application()
+app.include_router(exchangerates.router)
+
 
 @app.get("/")
 def get_root():
     return {"message": "Great, it works. Now you can check our endpoints."}
-
-
-@app.get("/average/{code}/{date}")
-def read_average(code: str, date: date):
-    url = f'http://api.nbp.pl/api/exchangerates/rates/A/{code}/{date}/?format=json'
-    req = requests.get(url)
-    response = req.json()
-    avg = response['rates'][0]['mid']
-    res = {"average": avg}
-    return res
-
-
-@app.get("/minmax/{code}/{topCount}")
-def read_minmax(code: str, topCount: int):
-    url = f"http://api.nbp.pl/api/exchangerates/rates/a/{code}/last/{topCount}/?format=json"
-    req = requests.get(url)
-    response = req.json()
-    minmax_list = [x['mid'] for x in response['rates']]
-    res = {'min': min(minmax_list), 'max': max(minmax_list)}
-    return res
-
-
-@app.get("/difference/{code}/{topCount}")
-def read_difference(code: str, topCount: int):
-    url = f"http://api.nbp.pl/api/exchangerates/rates/c/{code}/last/{topCount}/?format=json"
-
-    req = requests.get(url)
-    response = req.json()
-    difference: list = [round(x['ask'] - x['bid'], 4) for x in response['rates']]
-    max_diff = max(difference)
-    most_common_diff = Counter(difference).most_common(1)[0][0]
-    res = {"max difference": max_diff, "most common difference": most_common_diff}
-    return res
-
